974713240c2a679eba313a5a32a620fb579ad71c
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-17 13:14:31 +0530

    new background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
             alt=""
           />
 
-          <p className=" text-3xl font-bold font-groot-one text-[#000]">
+          <p className=" text-3xl font-bold font-groot-one text-[#fff]">
             GrootSol
           </p>
         </div>
