f8980755bf34b46ce4eafcf4327cb282c2efe709
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-17 22:15:14 +0530

    Update MemeCards.jsx

--- src/sections/MemeCards.jsx
+++ src/sections/MemeCards.jsx
@@ -7,6 +7,10 @@ function MemeCards() {
         Our Products
       </h2>
 
+      <p className=" text-center text-base">
+      Join us on this epic journey of growth and joy with GrootSol!
+      </p>
+
       <div className=" flex items-center gap-4 justify-between flex-col sm:flex-row">
         <div className="flex flex-col flex-wrap justify-center items-center overflow-hidden rounded-xl  w-[270px] h-auto sm:h-[500px]">
           <div className="w-[250px] mb-2 border-none rounded-2xl overflow-hidden">
@@ -19,7 +23,7 @@ function MemeCards() {
 
         <div className="flex flex-col flex-wrap  justify-center items-center overflow-hidden rounded-xl  w-[270px] h-auto sm:h-[500px]">
           <div className="w-[250px] mb-2 border-none rounded-2xl overflow-hidden">
-            <img src="images/MemesCardTwo.webp" alt="" className="" />
+          <img src="images/MemesCardFour.webp" alt="" className="" />
           </div>
           <div>
             <p className="font-groot-one text-xl">Groot Gaming</p>
@@ -41,7 +45,8 @@ function MemeCards() {
 
         <div className="flex flex-col flex-wrap justify-center items-center overflow-hidden rounded-xl  w-[270px] h-auto sm:h-[500px]">
           <div className="w-[250px] mb-2 border-none rounded-2xl overflow-hidden">
-            <img src="images/MemesCardFour.webp" alt="" className="" />
+           
+            <img src="images/MemesCardTwo.webp" alt="" className="" />
           </div>
           <div>
             <p className=" font-groot-one text-xl ">Groot Staking</p>
