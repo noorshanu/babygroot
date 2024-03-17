8fb2ae07467ef62a6cb40f1d0c7d332b624bfed2
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-17 22:08:48 +0530

    new

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
           <div className=" flex justify-between flex-col sm:flex-row items-center ">
             <div className=" w-full  bg-[#851B2C] bg-opacity-70 border-2 rounded-lg p-5 shadow-lg border-[#68c513]">
               <h1 className=" font-groot-one text-2xl sm:text-4xl my-4 text-[#fff]">
-                Welcome to the Groot NFT Universe! :
+              Experience GrootSol adventure: where growth, fun, and profits unite
               </h1>
 
               
@@ -15,18 +15,18 @@ function Hero() {
 
               <div className=" py-2">
                 <a
-                  href="https://solscan.io/token/Ft5kV9VpmNuFawoGLhbHKTsWDUKUraa43GKdQBf8zdV3"
+                  href="https://solscan.io/token/6VC94yLWrtigSdgRuiYqTR538P1gGEP3P1jjGGtSUC9X"
                   target="_blank"
                   className="my-2 font-groot-one flex justify-start text-xs  text-white  sm:text-lg"
                 >
                   {" "}
-                  Contract : Ft5kV9VpmNuFawoGLhbHKTsWDUKUraa43GKdQBf8zdV3
+                  Contract : 6VC94yLWrtigSdgRuiYqTR538P1gGEP3P1jjGGtSUC9X
                 </a>
               </div>
               <div className="relative z-40 flex items-center justify-start my-4 max-sm:flex-col max-sm:space-y-4 sm:space-x-5">
               
                 <a
-                  href="https://t.me/sushicatso"
+                  href="https://t.me/grootsol"
                   target="_blank"
                   className="text-base py-2 uppercase font-sushi-one px-10 rounded-full bg-[#f3a667] shadow-lg shadow-black/40 text-black  font-bold  border-[#68c513] border-2"
                 >
@@ -34,7 +34,7 @@ function Hero() {
                 </a>
 
                 <a
-                  href="https://www.dextools.io/app/en/solana/pair-explorer/FJky1Wejuou2Gr9bB16tiNiWjNCBLg7crZ9s9mttE58w?t=1710608652588"
+                  href="#"
                   target="_blank"
                   className="py-2 text-base uppercase font-sushi-one px-10 rounded-full bg-[#f3a667] shadow-lg shadow-black/40 text-black  font-bold border-2 border-[#68c513]"
                 >
--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -12,26 +12,21 @@ const Intro = () => {
         <div className="  px-4  py-2 flex justify-between flex-col sm:flex-row items-center ">
           <div className=" max-w-xl">
             <p className="font-medium font-groot-one text-xl sm:text-3xl text-white  py-2">
-              Introducing Baby Groot Coin: The Rootin'-Tootin' Crypto Companion
-              Inspired by the Guardian of the Galaxy!
+            Fellow adventurers, let's enter the GrootSol universe – where fun meets finance, and memes meet money!
             </p>
             <h1 className=" text-xl sm:text-2xl py-3  font-groot-one text-white  font-medium">
-              Groot NFTs: Step into the cosmic realm with our exclusive
-              collection of Groot NFTs. Each one as unique and endearing as our
-              beloved Baby Groot himself!
+            GrootSol is the ultimate crypto adventure inspired by the iconic figure of growth and innocence. As Grootsol grows larger, it produces profits for its community, known for its groot glory, sprout revolution, and personalized adventures.
             </h1>
 
             <h1 className=" text-xl sm:text-2xl py-3 font-groot-one  text-white font-medium">
-              GrootBot: Our trusty GrootBot is here to assist you in navigating
-              the crypto universe. Stay on top of market trends while grooving
-              to the beats of Groot's tunes!
+            Join us on this epic journey of growth and joy with GrootSol!
             </h1>
 
-            <h1 className=" text-xl sm:text-2xl py-3 font-groot-one text-white font-medium">
+            {/* <h1 className=" text-xl sm:text-2xl py-3 font-groot-one text-white font-medium">
               24/7 Galactic Community: Join our intergalactic community and
               let's grow together as we protect the galaxy and spread Groot's
               message of unity and friendship! We are Groot!
-            </h1>
+            </h1> */}
           </div>
 
           <div className=" w-full sm:w-[500px]">
